963aa1e87936072dcebee62895dbed21cfa38bcf
Aaron Bond <aaron.reid.bond@gmail.com>
2023-06-13 13:16:33 -0700

    hello world java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,37 @@
 public class HelloWorld {
     public static void  main(String[] args) {
         System.out.println("Hello World!");
+
+        int myFavoriteNumber = 42;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Dont forget your towel!";
+        System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int y = 5;
+//        System.out.println(++y);
+//        System.out.println(y);
+
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+
+        byte test = 127;
+        System.out.println(++test);
     }
 }
