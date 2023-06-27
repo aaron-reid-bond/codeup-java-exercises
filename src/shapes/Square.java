c05f591b9499b1eb605dc8b02438b91f937ac358
Aaron Bond <aaron.reid.bond@gmail.com>
2023-06-27 13:05:35 -0700

    measurable shape quad

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,16 +1,42 @@
 package shapes;
 
-public class Square extends Rectangle{
+public class Square extends Quadrilateral{
+    public Square(double i) {
+        super(i ,i);
+    }
 
-    protected double side;
-    public Square(double side) {
-        super(side, side);
-        this.side = side;
-    }
-    public double getArea() {
-        return side * side;
-    }
+    @Override
     public double getPerimeter() {
-        return 4 * side;
+        return 4 * length;
     }
+
+    @Override
+    public double getArea() {
+        return length * length;
+    }
+
+    @Override
+    public void setLength(double length) {
+        this.length = length;
+        this.width = length;
+    }
+
+    @Override
+    public void setWidth(double width) {
+        this.length = width;
+        this.width = width;
+    }
+
+
+//    protected double side;
+//    public Square(double side) {
+//        super(side, side);
+//        this.side = side;
+//    }
+//    public double getArea() {
+//        return side * side;
+//    }
+//    public double getPerimeter() {
+//        return 4 * side;
+//    }
 }
