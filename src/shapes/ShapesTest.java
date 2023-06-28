6573cb2a0d587ef2c8f71fdc4188bf61827d5172
Aaron Bond <aaron.reid.bond@gmail.com>
2023-06-28 09:23:12 -0700

    changes to shape test

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -2,11 +2,13 @@ package shapes;
 
 public class ShapesTest {
     public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(5, 4);
-        Square box2 = new Square(5);
+        Measurable box1 = new Rectangle(5, 4);
+
         System.out.println("box1.getArea() = " + box1.getArea());
         System.out.println("box1.getPerimeter() = " + box1.getPerimeter());
-        System.out.println("box2.getArea() = " + box2.getArea());
-        System.out.println("box2.getPerimeter() = " + box2.getPerimeter());
+
+        box1 = new Square(5);
+        System.out.println("box1.getArea() = " + box1.getArea());
+        System.out.println("box1.getPerimeter() = " + box1.getPerimeter());
     }
 }
